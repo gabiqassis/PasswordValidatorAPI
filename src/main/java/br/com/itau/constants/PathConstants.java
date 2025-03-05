ba5613764cf054b1273a5a85f59c2cf775e9417b
gabiqassis <gabriela-amora@outlook.com>
2025-03-05 10:50:28 -0300

    initial commit

---
+++ src/main/java/br/com/itau/constants/PathConstants.java
@@ -0,0 +1,11 @@
+package br.com.itau.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathConstants {
+    private static final String API = "/api";
+
+    public static final String VALIDATE_V1 = API + "/v1/validate";
+
+}
